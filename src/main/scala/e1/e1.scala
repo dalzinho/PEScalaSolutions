ab63b3f9d845479accd6475015113a6d4cce2f77
John Daly <jpdaly@gmail.com>
2017-10-17 06:00:54 +0100

    initial commit, add solution to e1

---
+++ src/main/scala/e1/e1.scala
@@ -0,0 +1,14 @@
+package e1
+
+object e1 extends App{
+  /*
+  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+  Find the sum of all the multiples of 3 or 5 below 1000.
+   */
+
+  val sum = (1 to 999).toList.filter(n => n % 3 == 0 || n % 5 == 0).sum
+
+  println(sum)
+
+}
